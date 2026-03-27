8e99be0ca3301c76733c7440c9ad34a98ab036e7
BomDia16 <arturbarbosaoliveira@gmail.com>
2026-03-26 23:01:33 -0300

    Primeira implementacao grafo

--- AlgoritimosFacul/src/Projeto/Livro.java
+++ AlgoritimosFacul/src/Projeto/Livro.java
@@ -11,4 +11,22 @@ public class Livro {
         this.autor = autor;
         this.ano_publicacao = ano_publicacao;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return id == livro.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return (this.titulo + " de " + this.autor + " " + this.ano_publicacao).toString();
+    }
 }
--- AlgoritimosFacul/src/Projeto/Main.java
+++ AlgoritimosFacul/src/Projeto/Main.java
@@ -1,28 +1,42 @@
 package Projeto;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        // Livro livro1 = new Livro(1, "Não sobrou nenhum", "Agatha Christie", 1930);
+        // LinkedList<Livro> livros = new LinkedList<>();
+        // livros.add(livro1);
+        // livros.add(new Livro(2, "titulo", "autor", 1900));
+
+        // Queue<Livro> filaLivros = new LinkedList<>();
+        // filaLivros.add(livro1);
+
+        // Stack<Livro> historicoLivros = new Stack<>();
+        // historicoLivros.add(livros.get(1));
+
+        // for (Livro livro : filaLivros) {
+        //     System.out.println(livro.titulo);
+        // }
+
+        // for (Livro livro : historicoLivros) {
+        //     System.out.println(livro.titulo);
+        // }
+
         Livro livro1 = new Livro(1, "Não sobrou nenhum", "Agatha Christie", 1930);
-        LinkedList<Livro> livros = new LinkedList<>();
-        livros.add(livro1);
-        livros.add(new Livro(2, "titulo", "autor", 1900));
+        Livro livro2 = new Livro(2, "titulo", "autor", 1900);
 
-        Queue<Livro> filaLivros = new LinkedList<>();
-        filaLivros.add(livro1);
+        HashMap<Livro, Set<Livro>> recomendacoes = new HashMap<>();
 
-        Stack<Livro> historicoLivros = new Stack<>();
-        historicoLivros.add(livros.get(1));
+        recomendacoes.putIfAbsent(livro1, new HashSet<>());
+        recomendacoes.get(livro1).add(livro2);
+        recomendacoes.putIfAbsent(livro2, new HashSet<>());
+        recomendacoes.get(livro2).add(livro1);
 
-        for (Livro livro : filaLivros) {
-            System.out.println(livro.titulo);
-        }
-
-        for (Livro livro : historicoLivros) {
-            System.out.println(livro.titulo);
+        for (Livro livro : recomendacoes.keySet()) {
+            System.out.println("Você leu: " + livro.titulo + ". Você pode gostar de: " + recomendacoes.get(livro).toString());
         }
     }
 }
